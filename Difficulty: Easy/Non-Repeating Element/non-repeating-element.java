5406c2b5ac1d22f46021bf9a13097cebd3ee8cbe
Anurag Singh Yadav <119932277+anuragyadav7905@users.noreply.github.com>
2026-03-16 10:54:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Non-Repeating Element/non-repeating-element.java
@@ -0,0 +1,17 @@
+// User function Template for Java
+
+class Solution {
+    public int firstNonRepeating(int[] arr) {
+        // Complete the function
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        }
+        for(int i=0;i<arr.length;i++){
+            if(map.get(arr[i])==1){
+                return arr[i];
+            }
+        }
+        return 0;
+    }
+}
